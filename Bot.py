dba11213ba30aaff7d41e8b25d3cf7ce65bc1483
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 09:46:22 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -16,7 +16,7 @@ def button_message(message):
  @bot.message_handler(content_types='text')
  def message_reply(message):
     if message.text=="Кнопка":
-        bot.send_message(message.chat.id, "https://yandex.ru")
+        bot.send_message(message.chat.id,"https://yandex.ru")
 
 if __name__ == '__main__':
      bot.infinity_polling()
