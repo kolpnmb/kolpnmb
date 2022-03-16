8f9b2e4060e9ba404638733387864be6ed9cde21
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 09:55:02 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,18 +1,28 @@
 import telebot
-token = '5132176469:AAHLroTt-bMBglESXe-dpyALqHYtHKJksYg'
+from telebot import types
+token = '5208733321:AAGqzpcwMQs3oM31FmE2Ns9HeCq9d3ExA0E'
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
-
+    
 @bot.message_handler(commands=['button'])
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1= types.KeyboardButton("Кнопка")
-    markup.add(item1)
+    item1= types.KeyboardButton("vk")
+    item2= types.KeyboardButton("instagram")
+    markup.add(item1),(item2)
     bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
     
+    
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="vk":
+        bot.send_message(message.chat.id,"https://vk.com/")
+    elif message.text=="instagram":
+        bot.send_message(message.chat.id,"https://www.instagram.com/")
 
 
 if __name__ == '__main__':
