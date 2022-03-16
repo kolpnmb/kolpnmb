77a82ec94f0a53d712ee2c5d80bc70e1f0d0d386
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 09:15:52 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
+    bot.send_message(message.chat.id,text="Привет, {0.first_name}!".format(message.from_user))
 
 if __name__ == '__main__':
      bot.infinity_polling()
