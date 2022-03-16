0fcbd76eb9c068e513a70038190076a173d68d3e
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 10:07:45 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -8,6 +8,13 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
 
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1= types.KeyboardButton("vk")
+    item2= types.KeyboardButton("instagram")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
 
 
 if __name__ == '__main__':
