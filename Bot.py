fcbf8bbbe6827b54817510e5fa35e9e554e17c5f
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 09:43:52 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,13 +6,17 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
 
-
-    @bot.message_handler(commands=['button'])
+@bot.message_handler(commands=['button'])
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1= types.KeyboardButton("Кнопка")
     markup.add(item1)
     bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
+    
+ @bot.message_handler(content_types='text')
+ def message_reply(message):
+    if message.text=="Кнопка":
+        bot.send_message(message.chat.id, "https://yandex.ru")
 
 if __name__ == '__main__':
      bot.infinity_polling()
