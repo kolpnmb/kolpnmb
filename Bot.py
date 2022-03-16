a9505ac6267e4f42c0b7fb57ce57f306c589b3f5
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 10:05:00 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,22 +7,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!". format(message.from_user))
-    
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1= types.KeyboardButton("vk")
-    item2= types.KeyboardButton("instagram")
-    markup.add(item1),(item2)
-    bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
-    
-    
-@bot.message_handler(content_types='text')
-def message_reply(message):
-    if message.text=="vk":
-        bot.send_message(message.chat.id,"https://vk.com/")
-    elif message.text=="instagram":
-        bot.send_message(message.chat.id,"https://www.instagram.com/")
+
 
 
 if __name__ == '__main__':
