617ec71cacaeaa3788f987e48cedadcfcca1baf2
kolpnmb <101693132+kolpnmb@users.noreply.github.com>
2022-03-16 08:58:48 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 import telebot
-token = '5208733321:AAGqzpcwMQs3oM31FmE2Ns9HeCq9d3ExA0E'
+token = '5132176469:AAHLroTt-bMBglESXe-dpyALqHYtHKJksYg'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
